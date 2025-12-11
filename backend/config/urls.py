f76fa1ed1596b834f0b147cb99ce4a0d8026d067
GsDesk <johelpilacuan2@gmail.com>
2025-12-10 22:15:30 -0500

    Force media serving via explicit re_path view

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,5 +9,12 @@ urlpatterns = [
     path('api/', include('normativas.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
 
